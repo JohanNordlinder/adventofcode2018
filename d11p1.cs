07e621b2321f806b3d74511a3a4fe2d5966ef36f
JohanNordlinder <johan_nordlinder@hotmail.com>
2018-12-10 20:52:16 +0100

    Prepare for day 11

---
+++ d11p1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2018
+{
+    [TestClass]
+    public class D11P1
+    {
+
+        [TestMethod]
+        public void TestRun()
+        {
+            var input = System.IO.File.ReadAllLines("d_11_t_1.txt").ToList();
+            Assert.AreEqual(1337, new Program().RunChallenge(input));
+        }
+
+        [TestMethod]
+        public void RealRun()
+        {
+            var input = System.IO.File.ReadAllLines("d_11.txt").ToList();
+            Console.WriteLine("Result: " + new Program().RunChallenge(input));
+        }
+
+        public class Program
+        {
+            public int RunChallenge(List<string> input)
+            {
+                return input.Count();
+            }
+        }
+    }
+}
